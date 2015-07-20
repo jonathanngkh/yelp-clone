390452ce62833d53428d3f9a89fee36a010439ea
Jonathan Ng <jonngkh@hotmail.com>
2015-07-20 16:12:00 +0100

    HAML and moving on to front end

--- spec/features/restaurants_feature_spec.rb
+++ spec/features/restaurants_feature_spec.rb
@@ -20,4 +20,15 @@ feature 'restaurants' do
       expect(page).not_to have_content('No restaurants yet')
     end
   end
+
+  context 'adding restaurants' do
+    scenario 'shows form then restaurant' do
+      visit '/restaurants'
+      click_link 'Add a restaurant'
+      fill_in 'Name', with: 'KFC'
+      click_button 'Create Restaurant'
+      expect(page).to have_content 'KFC'
+      expect(current_path).to eq '/restaurants'
+    end
+  end
 end
