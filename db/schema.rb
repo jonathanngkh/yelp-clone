d3c66ccf0380106b03d6fd00dd9ea5f3a6c58d5d
Jonathan Ng <jonngkh@hotmail.com>
2015-07-21 11:28:50 +0100

    reviews midway

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150720160030) do
+ActiveRecord::Schema.define(version: 20150721102511) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,13 @@ ActiveRecord::Schema.define(version: 20150720160030) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.text     "description"
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.text     "thoughts"
+    t.integer  "rating"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
