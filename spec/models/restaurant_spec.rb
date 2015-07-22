9f6d2d531dd65b027c8c010c6959e34b28c56e04
Jonathan Ng <jonngkh@hotmail.com>
2015-07-22 11:08:19 +0100

    finished b1

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -1,5 +1,4 @@
 require 'rails_helper'
-require 'spec_helper'
 
 RSpec.describe Restaurant, type: :model do
   it { is_expected.to have_many(:reviews).dependent(:destroy) }
@@ -8,5 +7,11 @@ RSpec.describe Restaurant, type: :model do
     restaurant = Restaurant.new(name: "kf")
     expect(restaurant).to have(1).error_on(:name)
     expect(restaurant).not_to be_valid
+  end
+
+  it 'is not valid unless it has a unique name' do
+    Restaurant.create(name: "Moe's tavern")
+    restaurant = Restaurant.new(name: "Moe's tavern")
+    expect(restaurant).to have(1).error_on(:name)
   end
 end
