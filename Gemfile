c25bd9e68b5c03af731b89ab18544dcf1e499857
Jonathan Ng <jonngkh@hotmail.com>
2015-07-22 15:12:31 +0100

    facebook login works

--- Gemfile
+++ Gemfile
@@ -16,6 +16,7 @@ gem 'coffee-rails', '~> 4.1.0'
 
 # User management
 gem 'devise'
+gem 'omniauth-facebook'
 
 # Use jquery as the JavaScript library
 gem 'jquery-rails'
--- config/routes.rb
+++ config/routes.rb
@@ -20,7 +20,7 @@ Rails.application.routes.draw do
     resources :reviews
   end
 
-  devise_for :users
+  devise_for :users, :controllers => { :omniauth_callbacks => "users/omniauth_callbacks" }
   # Example resource route with options:
   #   resources :products do
   #     member do
